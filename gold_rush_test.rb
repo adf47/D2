0cea8bcfa275727c83f764c0bc8a447c4f2c80a7
Antonino Febbraro <afebbraro@atsapllc.com>
2018-06-13 12:20:38 -0400

    Minor changes

--- gold_rush_test.rb
+++ gold_rush_test.rb
@@ -8,6 +8,7 @@ class GoldRushTest < Minitest::Test
 
     def test_check_args_invalid
         mock_sim = Minitest::Mock.new("Mock Sim")
+        mock_sim.expect :check_args, 1
         mock_sim.expect :show_usage_and_exit, 1
         #gold_rush.check_args()
         assert_equal mock_sim
